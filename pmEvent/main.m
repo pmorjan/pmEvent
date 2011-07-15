49cdf4ee472fe414b340171bc7c8bcd2d9f3a7ff
Peter Morjan <pmorjan@gmail.com>
2011-07-15 11:17:19 +0200

    Initial Commit

---
+++ pmEvent/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  pmEvent
+//
+//  Created by peter on 2011-07-15.
+//  Copyright 2011 NoWhere. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
